76df1a0139a8f7a89a1afa47ba9dbe2b5481a658
thrkrdk <thrkardak@gmail.com>
2023-09-22 17:43:19 +0300

    Attiributes model added, user model refactored

--- src/models/User.ts
+++ src/models/User.ts
@@ -12,14 +12,4 @@ const rootUrl = '/api/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
 }

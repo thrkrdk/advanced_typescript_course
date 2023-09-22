f1b97fb5d279161f1b313b556dcacd4871328207
thrkrdk <thrkardak@gmail.com>
2023-09-22 17:27:07 +0300

    sync-model-added

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,6 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
@@ -18,23 +17,5 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios.get(`/api/users/${this.get('id')}`).then(
-      (response: AxiosResponse): void => {
-        this.set(response.data);
-      }
-    );
-  }
-
-  save(): void {
-    const id = this.get('id');
-
-    if (id) {
-      axios.put(`/api/users/${id}`, this.data);
-    } else {
-      axios.post('/api/users', this.data);
-    }
   }
 }

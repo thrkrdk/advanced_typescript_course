c41949aa5724e5adfb600d6106f96e4e40e62a3e
thrkrdk <thrkardak@gmail.com>
2023-09-22 17:17:07 +0300

    eventing added, user model refactored

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 });
 
-user.save();  // network kısına bak
-// /mock altındaki user.jsonun içine bak
+user.events.on('change', () => {
+  console.log('change!');
+});
+
+user.events.trigger('change');
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
 
 interface UserProps {
   id?: number;
@@ -6,10 +7,8 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-  events: { [key: string]: Callback[] } = {};
+  public events: Eventing = new Eventing();
 
   constructor(private data: UserProps) {}
 
@@ -21,30 +20,12 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach((callback) => {
-      callback();
-    });
-  }
-
   fetch(): void {
-    axios
-      .get(`/api/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
+    axios.get(`/api/users/${this.get('id')}`).then(
+      (response: AxiosResponse): void => {
         this.set(response.data);
-      });
+      }
+    );
   }
 
   save(): void {

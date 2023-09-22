722e699d8193f20257a0de19f0c559c359d4e327
thrkrdk <thrkardak@gmail.com>
2023-09-22 16:32:53 +0300

    optional properties

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,13 +1,16 @@
 interface UserProps {
-    name: string;
-    age: number;
+  name?: string;
+  age?: number;
+}
+
+export class User {
+  constructor(private data: UserProps) {}
+
+  get(propName: string): number | string {
+    return this.data[propName];
   }
-  
-  export class User {
-    constructor(private data: UserProps) {}
-  
-    get(propName: string): number | string {
-      return this.data[propName];
-    }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
   }
-  
+}

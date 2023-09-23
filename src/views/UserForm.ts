d74417a0b567d114dfd9b62028aaa1fe1913dfb5
thrkrdk <thrkardak@gmail.com>
2023-09-23 13:09:29 +0300

    model properties added

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,24 +1,57 @@
+import { User } from '../models/User';
+
 export class UserForm {
-    constructor(public parent: Element | null) {
-        if (parent === null) {
-            throw new Error('Element not found');
-        }
-    }
-  
-    template(): string {
-      return `
-        <div>
-          <h1>User Form</h1>
-          <input />
-        </div>
-      `;
-    }
-  
-    render(): void {
-      const templateElement = document.createElement('template');
-      templateElement.innerHTML = this.template();
-  
-      this.parent?.append(templateElement.content);
+  constructor(public parent: Element|null, public model: User ) {
+    if (parent === null) {
+        throw new Error('Element not found');
     }
   }
-  
+
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onHeaderHover
+    };
+  }
+
+  onHeaderHover(): void {
+    console.log('H1 was hovered over');
+  }
+
+  onButtonClick(): void {
+    console.log('Hi there');
+  }
+
+  template(): string {
+    return `
+      <div>
+        <h1>User Form</h1>
+        <div>User name: ${this.model.get('name')}</div>
+        <div>User age: ${this.model.get('age')}</div>
+        <input />
+        <button>Click Me</button>
+      </div>
+    `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
+  render(): void {
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
+
+    this.parent?.append(templateElement.content);
+  }
+}

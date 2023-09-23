19db6c038f60ccdb494c33716f266746566698eb
thrkrdk <thrkardak@gmail.com>
2023-09-23 12:45:07 +0300

    extended user class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { User } from './models/User';
 
-const user = new User({id:1, name: 'new record', age: 0 });
+const user = new User({id:1, name: 'new record', age: 0 }); // uygulama hata verecek bir sonraki bölümde güncelleyeceğiz
 
 console.log(user.get('name'));
 

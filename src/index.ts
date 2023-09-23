6e664759ccd2402e6b9366934f73cec8e22e0a8a
thrkrdk <thrkardak@gmail.com>
2023-09-23 12:15:09 +0300

    user save added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1, name: 'newer name', age: 0 });
+const user = new User({ id: 1, name: 'newer name', age: 0 }); // uygulama çalışınca json dosyasını kotrol et
 
 user.on('save', () => {
   console.log(user);
